d6dbfc0fc3dd77ed840168c93d10dde98e59115c
Alexey <106263226+Liquid7707@users.noreply.github.com>
2023-10-22 16:30:49 +0200

    add a shopping-cart logic

--- order/product_page/static/product_page/product.js
+++ order/product_page/static/product_page/product.js
@@ -7,7 +7,32 @@ document.querySelectorAll('.card').forEach(card => {
           }, 2000); 
     });
   });
-  
+
+window.addEventListener('click', function(event) {
+  if (event.target.classList.contains('add-to-cart')) {
+    const card = event.target.closest(".card")
+    
+    const productInfo = {
+      title: card.querySelector('.card-title').textContent,
+      price: card.querySelector('.product-price').textContent,
+      image: card.querySelector('.product-img').src,
+      id: card.dataset.id
+    }
+    console.log(productInfo)
+    // Получите текущий список товаров в корзине из локального хранилища
+    let cartItems = JSON.parse(localStorage.getItem('cartItems')) || [];
+
+    // Добавьте новый товар в корзину
+    cartItems.push(productInfo);
+
+    // Сохраните обновленный список товаров в локальное хранилище
+    localStorage.setItem('cartItems', JSON.stringify(cartItems));
+    
+
 
     
+
+}})
+  
+
     
